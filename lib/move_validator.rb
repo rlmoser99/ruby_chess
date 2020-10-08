b85b21ea017fae838d200a47e633ccd4187ce7de
Rachel Moser <moser.rachel@gmail.com>
2020-10-07 23:10:51 -0500

    add verify possible moves

--- lib/move_validator.rb
+++ lib/move_validator.rb
@@ -8,20 +8,19 @@ class MoveValidator
     @possible_moves = moves
   end
 
-  def run_possibilities
+  def verify_possible_moves
     location = @current_piece.location
-    # During each iteration, king_location can not be a piece's capture
-    king_location = determine_king_location
+    king_location = find_king_location
     @possible_moves.select do |move|
       @possible_board.data[location[0]][location[1]] = nil
       @possible_board.data[move[0]][move[1]] = @current_piece
-      move.include?(5)
+      opponent_captures?(king_location)
     end
   end
 
   private
 
-  def determine_king_location
+  def find_king_location
     if @current_piece.color == :black
       @possible_board.black_king.location
     else
@@ -29,10 +28,13 @@ class MoveValidator
     end
   end
 
-  # def run_board
-  #   @possible_board.data.each do |row|
-  #     row.each do |square|
-  #     end
-  #   end
-  # end
+  def opponent_captures?(location)
+    @possible_board.data.none? do |row|
+      row.any? do |square|
+        next unless square && square.color != @current_piece.color
+
+        square.captures.include?(location)
+      end
+    end
+  end
 end
--- spec/move_validator_spec.rb
+++ spec/move_validator_spec.rb
@@ -6,34 +6,38 @@ require_relative '../lib/pieces/piece'
 
 RSpec.describe MoveValidator do
   require 'pry'
-  subject(:validator) { described_class.new(black_queen, board, [[1, 4], [3, 4], [4, 4], [5, 4], [2, 5]]) }
-  let(:black_queen) { instance_double(Piece, location: [2, 4], color: :black) }
-  let(:black_king) { instance_double(Piece, location: [0, 4], color: :black) }
-  let(:white_queen) { instance_double(Piece, location: [6, 4], color: :white) }
-  let(:board) { instance_double(Board) }
-  let(:data) do
-    [
-      [nil, nil, nil, nil, black_king, nil, nil, nil],
-      [nil, nil, nil, nil, nil, nil, nil, nil],
-      [nil, nil, nil, nil, black_queen, nil, nil, nil],
-      [nil, nil, nil, nil, nil, nil, nil, nil],
-      [nil, nil, nil, nil, nil, nil, nil, nil],
-      [nil, nil, nil, nil, nil, nil, nil, nil],
-      [nil, nil, nil, nil, white_queen, nil, nil, nil],
-      [nil, nil, nil, nil, nil, nil, nil, nil]
-    ]
-  end
 
-  before do
-    allow(board).to receive(:data).and_return(data)
-    allow(board).to receive(:black_king).and_return(black_king)
-    allow(board).to receive(:white_king).and_return(nil)
-  end
+  describe '#verify_possible_moves' do
+    context 'when black queens moves to [2, 5]' do
+      subject(:validator) { described_class.new(black_queen, board, [[1, 4], [3, 4], [4, 4], [5, 4], [2, 5]]) }
+      let(:black_queen) { instance_double(Piece, location: [2, 4], color: :black) }
+      let(:black_king) { instance_double(Piece, location: [0, 4], color: :black) }
+      let(:white_queen) { instance_double(Piece, location: [6, 4], color: :white) }
+      let(:board) { instance_double(Board) }
+      let(:data) do
+        [
+          [nil, nil, nil, nil, black_king, nil, nil, nil],
+          [nil, nil, nil, nil, nil, nil, nil, nil],
+          [nil, nil, nil, nil, black_queen, nil, nil, nil],
+          [nil, nil, nil, nil, nil, nil, nil, nil],
+          [nil, nil, nil, nil, nil, nil, nil, nil],
+          [nil, nil, nil, nil, nil, nil, nil, nil],
+          [nil, nil, nil, nil, white_queen, nil, nil, nil],
+          [nil, nil, nil, nil, nil, nil, nil, nil]
+        ]
+      end
 
-  describe '#run_possibilities' do
-    it 'returns' do
-      result = validator.run_possibilities
-      expect(result).to contain_exactly([1, 4], [3, 4], [4, 4], [5, 4])
+      before do
+        allow(board).to receive(:data).and_return(data)
+        allow(board).to receive(:black_king).and_return(black_king)
+        allow(board).to receive(:white_king).and_return(nil)
+        allow(white_queen).to receive(:captures).and_return([], [], [], [], [[0, 4]])
+      end
+
+      it 'returns' do
+        results = validator.verify_possible_moves
+        expect(results).to contain_exactly([1, 4], [3, 4], [4, 4], [5, 4])
+      end
     end
   end
 end
