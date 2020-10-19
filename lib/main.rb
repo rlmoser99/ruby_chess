d67efc2108744807b1abb66cea6bf1f6144f6c4a
Rachel Moser <moser.rachel@gmail.com>
2020-10-19 16:29:28 -0500

    add more tests to move validator

--- lib/main.rb
+++ lib/main.rb
@@ -20,13 +20,10 @@ game.play
 
 # REFACTORING NOTES:
 # Board class is too big. Is there an abstraction or way that can reduce it?
-# Tests for move_validator may not be testing accurately, due to stubbing?
-# -> Test method that is stubbed, because it's easier to test that one.
 # Create 2 arrays of black & white pieces?
 # En_Passant deletes observer in two locations. Verify if both are needed.
 # Add a way to end the game early (resign)
 # King#find_possible_moves(board) rubocop warning
-# Refactor double iterator methods like: pieces = board.data.flatten(1).compact
 
 # RANDOM COMPUTER PLAYER:
 
