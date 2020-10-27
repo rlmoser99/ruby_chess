a4e5e4c0bfb3e126d13cfca6c80b4642b3880e60
Rachel Moser <moser.rachel@gmail.com>
2020-10-27 13:06:45 -0500

    add new pieces for each context

--- spec/move_validator_spec.rb
+++ spec/move_validator_spec.rb
@@ -67,43 +67,24 @@ RSpec.describe MoveValidator do
       end
     end
 
-    # [-----, -----, -----, -----, -----, -----, -----, -----],
-    # [-----, -----, -----, -----, -----, -----, -----, -----],
-    # [-----, -----, -----, -----, -----, -----, -----, wquen],
-    # [-----, -----, -----, -----, wnite, -----, bquen, -----],
-    # [-----, -----, -----, -----, -----, wrook, -----, -----],
-    # [-----, -----, -----, -----, -----, -----, -----, wbshp],
-    # [-----, -----, -----, -----, wpawn, -----, -----, -----],
-    # [-----, -----, -----, -----, -----, -----, wking, -----]
     context 'when king is in check at start of turn and 5 of 6 piece have valid moves' do
-      board = Board.new
-      board.data[2][7] = Queen.new(board, { color: :white, location: [2, 7] })
-      board.data[3][4] = Knight.new(board, { color: :white, location: [3, 4] })
-      board.data[3][6] = Queen.new(board, { color: :black, location: [3, 6] })
-      board.data[4][5] = Rook.new(board, { color: :white, location: [4, 5] })
-      board.data[5][7] = Bishop.new(board, { color: :white, location: [5, 7] })
-      board.data[7][6] = King.new(board, { color: :white, location: [7, 6] })
-      board.data[6][4] = Pawn.new(board, { color: :white, location: [6, 4] })
-
-      before do
-        board.instance_variable_set(:@white_king, board.data[7][6])
-      end
-
-      context 'when white pawn can not block the black queen' do
-        subject(:pawn_validator) { described_class.new([6, 4], board, [[5, 4], [4, 4]]) }
-
-        it 'has no moves' do
-          board.instance_variable_set(:@white_king, board.data[7][6])
-          pawn_validator.instance_variable_set(:@current_piece, board.data[6][4])
-          results = pawn_validator.verify_possible_moves
-          expect(results).to be_empty
-        end
-      end
-
+      # [-----, -----, -----, -----, -----, -----, -----, -----],
+      # [-----, -----, -----, -----, -----, -----, -----, -----],
+      # [-----, -----, -----, -----, -----, -----, -----, wquen],
+      # [-----, -----, -----, -----, wnite, -----, bquen, -----],
+      # [-----, -----, -----, -----, -----, wrook, -----, -----],
+      # [-----, -----, -----, -----, -----, -----, -----, wbshp],
+      # [-----, -----, -----, -----, wpawn, -----, -----, -----],
+      # [-----, -----, -----, -----, -----, -----, wking, -----]
       context 'when white queen can kill black queen' do
+        board = Board.new
+        board.data[2][7] = Queen.new(board, { color: :white, location: [2, 7] })
+        board.data[3][6] = Queen.new(board, { color: :black, location: [3, 6] })
+        board.data[7][6] = King.new(board, { color: :white, location: [7, 6] })
         subject(:validator) { described_class.new([2, 7], board, [[1, 7], [1, 6], [2, 6], [3, 6], [3, 7]]) }
 
         it 'returns move to kill the queen' do
+          board.instance_variable_set(:@white_king, board.data[7][6])
           validator.instance_variable_set(:@current_piece, board.data[2][7])
           results = validator.verify_possible_moves
           expect(results).to contain_exactly([3, 6])
@@ -111,9 +92,14 @@ RSpec.describe MoveValidator do
       end
 
       context 'when white knight can block the black queen' do
+        board = Board.new
+        board.data[3][4] = Knight.new(board, { color: :white, location: [3, 4] })
+        board.data[3][6] = Queen.new(board, { color: :black, location: [3, 6] })
+        board.data[7][6] = King.new(board, { color: :white, location: [7, 6] })
         subject(:validator) { described_class.new([3, 4], board, [[4, 6], [2, 6], [1, 5], [5, 5]]) }
 
         it 'returns move to block the queen' do
+          board.instance_variable_set(:@white_king, board.data[7][6])
           validator.instance_variable_set(:@current_piece, board.data[3][4])
           results = validator.verify_possible_moves
           expect(results).to contain_exactly([4, 6])
@@ -121,9 +107,14 @@ RSpec.describe MoveValidator do
       end
 
       context 'when white rook can block the black queen' do
+        board = Board.new
+        board.data[4][5] = Rook.new(board, { color: :white, location: [4, 5] })
+        board.data[3][6] = Queen.new(board, { color: :black, location: [3, 6] })
+        board.data[7][6] = King.new(board, { color: :white, location: [7, 6] })
         subject(:validator) { described_class.new([4, 5], board, [[4, 0], [4, 1], [4, 3], [4, 4], [4, 6], [4, 7]]) }
 
         it 'returns move to block the queen' do
+          board.instance_variable_set(:@white_king, board.data[7][6])
           validator.instance_variable_set(:@current_piece, board.data[4][5])
           results = validator.verify_possible_moves
           expect(results).to contain_exactly([4, 6])
@@ -131,9 +122,14 @@ RSpec.describe MoveValidator do
       end
 
       context 'when white bishop can block the black queen' do
+        board = Board.new
+        board.data[5][7] = Bishop.new(board, { color: :white, location: [5, 7] })
+        board.data[3][6] = Queen.new(board, { color: :black, location: [3, 6] })
+        board.data[7][6] = King.new(board, { color: :white, location: [7, 6] })
         subject(:validator) { described_class.new([5, 7], board, [[4, 6], [6, 6], [7, 5]]) }
 
         it 'returns two moves to block the queen' do
+          board.instance_variable_set(:@white_king, board.data[7][6])
           validator.instance_variable_set(:@current_piece, board.data[5][7])
           results = validator.verify_possible_moves
           expect(results).to contain_exactly([4, 6], [6, 6])
@@ -141,12 +137,31 @@ RSpec.describe MoveValidator do
       end
 
       context 'when white king can move out of check' do
+        board = Board.new
+        board.data[7][6] = King.new(board, { color: :white, location: [7, 6] })
+        board.data[3][6] = Queen.new(board, { color: :black, location: [3, 6] })
         subject(:validator) { described_class.new([7, 6], board, [[7, 5], [7, 7], [6, 5], [6, 6], [6, 7]]) }
 
         it 'returns four legal moves' do
+          board.instance_variable_set(:@white_king, board.data[7][6])
           validator.instance_variable_set(:@current_piece, board.data[7][6])
           results = validator.verify_possible_moves
           expect(results).to contain_exactly([7, 5], [7, 7], [6, 5], [6, 7])
+        end
+      end
+
+      context 'when white pawn can not block the black queen' do
+        board = Board.new
+        board.data[6][4] = Pawn.new(board, { color: :white, location: [6, 4] })
+        board.data[3][6] = Queen.new(board, { color: :black, location: [3, 6] })
+        board.data[7][6] = King.new(board, { color: :white, location: [7, 6] })
+        subject(:pawn_validator) { described_class.new([6, 4], board, [[5, 4], [4, 4]]) }
+
+        it 'has no moves' do
+          board.instance_variable_set(:@white_king, board.data[7][6])
+          pawn_validator.instance_variable_set(:@current_piece, board.data[6][4])
+          results = pawn_validator.verify_possible_moves
+          expect(results).to be_empty
         end
       end
     end
