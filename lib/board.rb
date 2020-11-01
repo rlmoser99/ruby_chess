5c4ec145abc0d39a0674ac7f1d4994967ea8c108
Rachel Moser <moser.rachel@gmail.com>
2020-11-01 15:22:28 -0600

    refactor tests

--- lib/board.rb
+++ lib/board.rb
@@ -50,6 +50,27 @@ class Board
     reset_board_values
   end
 
+  # creates movement strategy based on the coordinates of the move
+  def create_movement(coords)
+    if en_passant_capture?(coords)
+      EnPassantMovement.new
+    elsif pawn_promotion?(coords)
+      PawnPromotionMovement.new
+    elsif castling?(coords)
+      CastlingMovement.new
+    else
+      BasicMovement.new
+    end
+  end
+
+  # script to reset for next turn and notify pieces to update moves/captures
+  def reset_board_values
+    @previous_piece = @active_piece
+    @active_piece = nil
+    changed
+    notify_observers(self)
+  end
+
   # returns true if active piece can capture the previous piece en passant
   def possible_en_passant?
     @active_piece&.captures&.include?(@previous_piece&.location) &&
@@ -150,19 +171,6 @@ class Board
     pieces.each { |piece| piece.update(self) }
   end
 
-  # creates movement strategy based on the coordinates of the move
-  def create_movement(coords)
-    if en_passant_capture?(coords)
-      EnPassantMovement.new
-    elsif pawn_promotion?(coords)
-      PawnPromotionMovement.new
-    elsif castling?(coords)
-      CastlingMovement.new
-    else
-      BasicMovement.new
-    end
-  end
-
   # returns true when coords are a pawn captured en_passant
   def en_passant_capture?(coords)
     @previous_piece&.location == [coords[:row], coords[:column]] &&
@@ -178,14 +186,6 @@ class Board
   def castling?(coords)
     file_difference = (coords[:column] - @active_piece.location[1]).abs
     @active_piece&.symbol == " \u265A " && file_difference == 2
-  end
-
-  # script to reset for next turn and notify pieces to update moves/captures
-  def reset_board_values
-    @previous_piece = @active_piece
-    @active_piece = nil
-    changed
-    notify_observers(self)
   end
 
   # returns true if color of active piece matches the cooresponding rank
--- spec/notation_translator_spec.rb
+++ spec/notation_translator_spec.rb
@@ -6,52 +6,68 @@ RSpec.describe NotationTranslator do
   subject(:translator) { described_class.new }
 
   describe '#translate_position' do
-    it 'returns row:0 column:0' do
-      user_input = 'a8'
-      result = translator.translate_notation(user_input)
-      expect(result).to eq({ row: 0, column: 0 })
+    context 'when user input is a8' do
+      it 'returns row:0 column:0' do
+        user_input = 'a8'
+        result = translator.translate_notation(user_input)
+        expect(result).to eq({ row: 0, column: 0 })
+      end
     end
 
-    it 'returns row:1 column:1' do
-      user_input = 'b7'
-      result = translator.translate_notation(user_input)
-      expect(result).to eq({ row: 1, column: 1 })
+    context 'when user input is b7' do
+      it 'returns row:1 column:1' do
+        user_input = 'b7'
+        result = translator.translate_notation(user_input)
+        expect(result).to eq({ row: 1, column: 1 })
+      end
     end
 
-    it 'returns row:2 column:2' do
-      user_input = 'c6'
-      result = translator.translate_notation(user_input)
-      expect(result).to eq({ row: 2, column: 2 })
+    context 'when user input is c6' do
+      it 'returns row:2 column:2' do
+        user_input = 'c6'
+        result = translator.translate_notation(user_input)
+        expect(result).to eq({ row: 2, column: 2 })
+      end
     end
 
-    it 'returns row:3 column:3' do
-      user_input = 'd5'
-      result = translator.translate_notation(user_input)
-      expect(result).to eq({ row: 3, column: 3 })
+    context 'when user input is d5' do
+      it 'returns row:3 column:3' do
+        user_input = 'd5'
+        result = translator.translate_notation(user_input)
+        expect(result).to eq({ row: 3, column: 3 })
+      end
     end
 
-    it 'returns row:4 column:4' do
-      user_input = 'e4'
-      result = translator.translate_notation(user_input)
-      expect(result).to eq({ row: 4, column: 4 })
+    context 'when user input is e4' do
+      it 'returns row:4 column:4' do
+        user_input = 'e4'
+        result = translator.translate_notation(user_input)
+        expect(result).to eq({ row: 4, column: 4 })
+      end
     end
 
-    it 'returns row:5 column:5' do
-      user_input = 'f3'
-      result = translator.translate_notation(user_input)
-      expect(result).to eq({ row: 5, column: 5 })
+    context 'when user input is f3' do
+      it 'returns row:5 column:5' do
+        user_input = 'f3'
+        result = translator.translate_notation(user_input)
+        expect(result).to eq({ row: 5, column: 5 })
+      end
     end
 
-    it 'returns row:6 column:6' do
-      user_input = 'g2'
-      result = translator.translate_notation(user_input)
-      expect(result).to eq({ row: 6, column: 6 })
+    context 'when user input is g2' do
+      it 'returns row:6 column:6' do
+        user_input = 'g2'
+        result = translator.translate_notation(user_input)
+        expect(result).to eq({ row: 6, column: 6 })
+      end
     end
 
-    it 'returns row:7 column:7' do
-      user_input = 'h1'
-      result = translator.translate_notation(user_input)
-      expect(result).to eq({ row: 7, column: 7 })
+    context 'when user input is h1' do
+      it 'returns row:7 column:7' do
+        user_input = 'h1'
+        result = translator.translate_notation(user_input)
+        expect(result).to eq({ row: 7, column: 7 })
+      end
     end
   end
 end
