23f552da4354b65fdc36564b70953e133e1c2d6b
Rachel Moser <moser.rachel@gmail.com>
2020-11-04 12:24:03 -0600

    fix en passant bug that put king in check

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -32,7 +32,7 @@ class Pawn < Piece
     [
       basic_capture(board, file - 1),
       basic_capture(board, file + 1),
-      en_passant_capture(board.previous_piece)
+      en_passant_capture(board)
     ].compact
   end
 
@@ -71,22 +71,40 @@ class Pawn < Piece
     return [rank, file] if opposing_piece?(rank, file, board.data)
   end
 
-  def en_passant_capture(previous_piece)
-    capture = previous_piece&.location
+  def en_passant_capture(board)
+    capture = board.previous_piece&.location
     return unless capture
 
     column_difference = (@location[1] - capture[1]).abs
     return unless column_difference == 1
 
-    return capture if valid_en_passant?(previous_piece)
+    return capture if valid_en_passant?(board)
   end
 
   def update_en_passant(row)
     @en_passant = (row - location[0]).abs == 2
   end
 
-  def valid_en_passant?(piece)
-    en_passant_rank? && symbol == piece.symbol && piece.en_passant
+  def valid_en_passant?(board)
+    en_passant_rank? &&
+      symbol == board.previous_piece.symbol &&
+      board.previous_piece.en_passant &&
+      legal_en_passant_move?(board)
+  end
+
+  # checks if the en passant move & capture will not leave the King in check
+  def legal_en_passant_move?(board)
+    pawn_location = board.previous_piece.location
+    en_passant_move = [pawn_location[0], pawn_location[1] + rank_direction]
+    temp_board = remove_captured_en_passant_pawn(board, pawn_location)
+    legal_capture = remove_illegal_moves(temp_board, en_passant_move)
+    legal_capture.size.positive?
+  end
+
+  def remove_captured_en_passant_pawn(board, pawn_location)
+    temp_board = Marshal.load(Marshal.dump(board))
+    temp_board.data[pawn_location[0]][pawn_location[1]] = nil
+    temp_board
   end
 
   def move_set; end
